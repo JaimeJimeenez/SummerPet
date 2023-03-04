2535cca4a7fa7a652d34565412e00e3ad492394d
Jaime Jimenez <jaimejimenez99@hotmail.com>
2023-03-04 23:35:52 +0100

    Changed DAOUsuario

--- SMPT/Fuentes/Prototipo Tecnologico/Src/DAOs/DAOUsuario.js
+++ SMPT/Fuentes/Prototipo Tecnologico/Src/DAOs/DAOUsuario.js
@@ -4,13 +4,13 @@ class DAOUsuario {
 
     constructor(pool) { this.pool = pool; }
 
-    searchCuidadorByKeyWord(keyWord, callback) {
+    searchByKeyWord(keyWord, callback) {
         this.pool.getConnection((err, connection) => {
             if (err) callback(new Error('No se pudo conectar a la base de datos: ' + err.message));
             else {
                 const sql = 'SELECT * FROM Usuario Where instr(Nombre, ?) or instr(Telefono, ?) or instr(Email, ?) or instr(Direccion, ?) > 0;';
 
-                connection.query(sql, [keyWord], (err, rows) => {
+                connection.query(sql, [keyWord, keyWord, keyWord, keyWord], (err, rows) => {
                     connection.release();
                     if (err) callback(new Error('No se pudo acceder a la base de datos: ' + err.message));
                     else callback(null, rows);
@@ -19,3 +19,5 @@ class DAOUsuario {
         });
     }
 }
+
+module.exports = DAOUsuario;
