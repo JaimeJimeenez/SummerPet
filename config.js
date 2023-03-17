ea48aeee4c0645d12329f2f1849e20730d5796b5
Jaime Jimenez <jaimejimenez99@hotmail.com>
2023-03-17 17:41:48 +0100

    Changed database configuration

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 'use strict'
+const fs = require('fs');
 
 module.exports = {
 
-    mysqlConfig : {
-        host : 'localhost',
-        user : 'root',
-        password : '',
-        database : 'SummerPet'
+    mysqlConfig :{
+        host: 'summerpet.mysql.database.azure.com',
+        user: 'SMPT',
+        password: 'SummerPet2023',
+        database: 'summerpet',
+        port: 3306,
+        ssl: {ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")}
     },
-
-    port : 3000
     
-};
+    port: 3000
+}
