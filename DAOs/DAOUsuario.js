617cd186d8df4b8e31c6391ceada2955d182dc5c
Jaime Jimenez <jaimejimenez99@hotmail.com>
2023-03-18 11:39:24 +0100

    Added dao method

--- DAOs/DAOUsuario.js
+++ DAOs/DAOUsuario.js
@@ -4,11 +4,26 @@ class DAOUsuario {
 
     constructor(pool) { this.pool = pool; }
 
+    getUser(id, callback) {
+        this.pool.getConnection((err, connection) => {
+            if (err) callback(new Error('No se pudo conectar a la base de datos: ' + err.message));
+            else {
+                const sql = 'SELECT * FROM User Where Id = ?';
+
+                connection.query(sql, [id], (err, user) => {
+                    connection.release();
+                    if (err) callback(new Error('No se pudo acceder a la base de datos: ' + err.message));
+                    else callback(null, user);
+                });
+            }
+        });
+    }
+
     searchByKeyWord(keyWord, callback) {
         this.pool.getConnection((err, connection) => {
             if (err) callback(new Error('No se pudo conectar a la base de datos: ' + err.message));
             else {
-                const sql = 'SELECT * FROM Usuario Where instr(Nombre, ?) or instr(Telefono, ?) or instr(Email, ?) or instr(Direccion, ?) > 0;';
+                const sql = 'SELECT * FROM User Where instr(Name, ?) or instr(Phone, ?) or instr(Email, ?) or instr(Direction, ?) > 0;';
 
                 connection.query(sql, [keyWord, keyWord, keyWord, keyWord], (err, rows) => {
                     connection.release();
