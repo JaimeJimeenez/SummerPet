4fe734dfd60c3c67de54faf4a826af7596f06851
xlenyit <elencari@ucm.es>
2023-03-25 23:25:34 +0100

    jest tests passed

--- tests/seePetsAccommodation.test.js
+++ tests/seePetsAccommodation.test.js
@@ -4,21 +4,24 @@ const mysql = require('mysql');
 const config = require('../config');
 
 const DAOUsuario = require('../DAOs/DAOUsuario');
-
+const {render,screen} = require('@testing-library/jest-dom')
 const pool = mysql.createPool(config.mysqlConfig);
 const daoUsuario = new DAOUsuario(pool);
 
 describe('see carers accommodation for the pet test', () => {
-  /*test('Displays 2 pictures uploaded by Jaime', done => {
+  test('Displays 2 pictures uploaded by Jaime', done => {
     function callback(error, data) {
       if (error) {
         done(error);
         return;
       }
       try {
-        const usuario = data.find(u => u.Name === 'Jaime');
-        expect(usuario).toBeDefined();
-        console.log(data);
+        
+        expect(data).toBeDefined();
+        expect(data.length).toBe(2);
+        data.forEach(photo => {
+          expect(photo.Id).toBe(1); // verificación del ID del usuario
+        });
         done();
       } catch(error) {
         done(error);
@@ -26,7 +29,33 @@ describe('see carers accommodation for the pet test', () => {
     }
 
     daoUsuario.getPhotosLocation(1,callback);
-  });*/
+  });
+
+ test('Displays an error message in Marias profile', done => {
+    function callback(error, data) {
+      if (error) {
+        done(error);
+        return;
+      }
+      try {
+        
+        expect(data).toBeDefined();
+        expect(data).toStrictEqual([]);
+        const usuario = { nombre: 'Maria' }; // Simula usuario sin Photos
+        const mensajeError = 'No existen fotos de momento';
+        expect(() => {
+          // Función que verifica que se muestra el mensaje de error
+          if (data.length === 0) throw new Error(mensajeError);
+        }).toThrow(mensajeError);
+        done();
+       
+      } catch(error) {
+        done(error);
+      }
+    }
+
+    daoUsuario.getPhotosLocation(2,callback);
+  });
 
   
 });
