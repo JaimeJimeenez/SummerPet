15f73e0311ae09310ba342613db515f5232c6f15
Ivanpisonero <ivanpiso@ucm.es>
2023-04-13 15:00:57 +0200

    Tests unitarios ver especialidades

--- tests/viewSpecialties.test.js
+++ tests/viewSpecialties.test.js
@@ -8,7 +8,7 @@ const DAOUser = require('../DAOs/DAOUser');
 const pool = mysql.createPool(config.mysqlConfig);
 const daoUser = new DAOUser(pool);
 
-test('No specialties', done => {
+test('No dog breeds', done => {
     function callback(error, data) {
         if (error) {
             done(error);
@@ -23,5 +23,68 @@ test('No specialties', done => {
         }
     }
 
-    daoUsuario.getSpecialties(3, callback);
+    daoUser.getDogBreeds(3, callback);
+});
+
+test('No dog sizes', done => {
+    function callback(error, data) {
+        if (error) {
+            done(error);
+            return;
+        }
+        
+        try {
+            expect(data).toStrictEqual([]);    
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+
+    daoUser.getDogSizes(3, callback);
+});
+
+test('Multipe dog breeds', done => {
+    function callback(error, data) {
+        if (error) {
+            done(error);
+            return;
+        }
+        
+        try {
+            var num_breeds = 5;
+            var resultado = Object.assign({}, data);
+            var index = 0;
+            while(index < num_breeds) {
+                resultado[index] = Object.assign({}, resultado[index]);
+                index++;
+            };
+            
+            expect(resultado).toEqual({"0" : {"Name": "Husky"}, "1": {"Name": "Golden Terrier"}, "2": {"Name": "Salchicha"}, "3": {"Name": "Golden Retriever"}, "4": {"Name": "Labrador"}});
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+
+    daoUser.getDogBreeds(1, callback);
+});
+
+test('Multipe dog sizes', done => {
+    function callback(error, data) {
+        if (error) {
+            done(error);
+            return;
+        }
+
+        try {
+            var resultado = Object.assign({}, data);
+            expect(resultado).toEqual({"0": {"Size": "pequenio"}, "1": {"Size": "grande"}, "2": {"Size": "mediano"}});    
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+
+    daoUser.getDogSizes(1, callback);
 });
