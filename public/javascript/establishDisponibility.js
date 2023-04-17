1079b8fe01bbeb94c1a18e1644b588214b327602
Jaime Jimenez <jaimejimenez99@hotmail.com>
2023-04-17 17:42:46 +0200

    Location Photos design

--- public/javascript/establishDisponibility.js
+++ public/javascript/establishDisponibility.js
@@ -2,45 +2,44 @@
 
 $(function() {
 
-    $('input[name="datefilter"]').daterangepicker({
-      autoUpdateInput: false,
-      locale: {
-          cancelLabel: 'Clear',
-          format: "DD/MM/YYYY",
-        separator: " - ",
-        applyLabel: "Aplicar",
-        cancelLabel: "Cancelar",
-        fromLabel: "De",
-        toLabel: "A",
-        customRangeLabel: "Personalizar",
-        weekLabel: "S",
-        daysOfWeek: [
-            "Do",
-            "Lu",
-            "Ma",
-            "Mi",
-            "Ju",
-            "Vi",
-            "Sa"
-        ],
-        monthNames: [
-            "Enero",
-            "Febrero",
-            "Marzo",
-            "Abril",
-            "Mayo",
-            "Junio",
-            "Julio",
-            "Agosto",
-            "Septiembre",
-            "Octubre",
-            "Noviembre",
-            "Diciembre"
-        ],
-        firstDay: 1
-        },
-        minDate: moment().startOf('day')
-        // alwaysShowCalendars: true
+  $('input[name="datefilter"]').daterangepicker({
+    autoUpdateInput: false,
+    locale: {
+      cancelLabel: 'Clear',
+      format: "DD/MM/YYYY",
+      separator: " - ",
+      applyLabel: "Aplicar",
+      cancelLabel: "Cancelar",
+      fromLabel: "De",
+      toLabel: "A",
+      customRangeLabel: "Personalizar",
+      weekLabel: "S",
+      daysOfWeek: [
+        "Do",
+        "Lu",
+        "Ma",
+        "Mi",
+        "Ju",
+        "Vi",
+        "Sa"
+    ],
+    monthNames: [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    ],
+    firstDay: 1
+    },
+    minDate: moment().startOf('day')    
     });
 
     $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
